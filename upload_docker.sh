58f878844e2471473853600257f90ec4c7ce04e6
gitcoleli <git@cole.li>
2021-02-21 22:46:04 +0000

    updating outputs

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="gitcoleli/markapp"
 
 # Step 2:  
 # Authenticate & tag
+docker login -u gitcoleli
+docker tag markapp $dockerpath
 echo "Docker ID and Image: $dockerpath"
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
